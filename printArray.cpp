30adf81c692c1f30b2cbe24f4f0eee154591aefb
misbanazir <misbanazir3@gmail.com>
2022-08-25 23:59:34 +0530

    orint array

---
+++ printArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[5] = {4,5,3,6,3};
+    for(int i = 0; i <= 5; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    return 0;
+}
